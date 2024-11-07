07a8dd3767acb58196f74b382bd1a30b1b1cd6e6
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-11-07 18:58:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "YASIYA-MD~9fEkxJoK#DCou9qGFKBNFihLLZXzsq3nWs3yJX1Z6H-0N8mO0YkA",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
